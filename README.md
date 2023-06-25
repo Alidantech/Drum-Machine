8f5b003da70d05384135269721e1415d65235383
PETER IRUNGU <peteralidante254@gmail.com>
2023-06-25 19:49:06 +0300

    complete drum machine

---
+++ README.md
@@ -0,0 +1,68 @@
+# Drum Machine
+
+This is a drum machine app built with React for the Drum Machine challenge on [freeCodeCamp](https://www.freecodecamp.org/). It allows users to play drum sounds by clicking on the drum pads or using keyboard keys.
+
+![Drum Machine Screenshot](screenshot.png)
+
+## Features
+
+- Nine drum pads, each with a unique sound and corresponding keyboard key
+- Clicking on a drum pad triggers the associated sound
+- Pressing the keyboard key associated with a drum pad triggers the sound
+- Display shows the description of the triggered sound
+- Responsive layout with Bootstrap styling
+
+## Technologies Used
+
+- React
+- HTML5
+- CSS3
+- Bootstrap
+
+## Getting Started
+
+To get a local copy of the project up and running, follow these steps:
+
+1. Clone the repository:
+
+```bash
+      git clone https://github.com/your-username/drum-machine.git
+```
+
+
+
+2. Change to the project directory:
+
+```bash
+      cd drum-machine
+```
+
+3. Install the dependencies:
+
+```bash
+      npm install
+```
+
+4. Start the development server:
+
+```
+      npm start
+```
+
+5. Open the app in your browser:
+
+      http://localhost:3000
+
+## Usage
+
+- Click on the drum pads to play the associated sounds.
+- Press the keyboard keys (Q, W, E, A, S, D, Z, X, C) to play the corresponding sounds.
+- The description of the triggered sound will be displayed in the center of the app.
+
+## Credits
+
+- Drum sounds: [freeCodeCamp](https://www.freecodecamp.org/)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE)
---
+++ drum-machine/src/App.js
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const DrumPad = ({ id, src, text, onClick }) => (
+  <div className="drum-pad btn btn-secondary" id={`${id}`} onClick={onClick}>
+    {text}
+    <audio className="clip" id={id} src={src}></audio>
+  </div>
+);
+
+const App = () => {
+  const [displayText, setDisplayText] = useState('');
+  const [volume, setVolume] = useState(0.5);
+
+  const handlePadClick = (event) => {
+    const audio = event.target.querySelector('audio');
+    audio.currentTime = 0;
+    audio.volume = volume;
+    audio.play();
+    setDisplayText(event.target.id);
+  };
+
+  const handleKeyDown = (event) => {
+    const drumPad = document.getElementById(event.key.toUpperCase());
+    if (drumPad) {
+      const audio = drumPad.querySelector('audio');
+      audio.currentTime = 0;
+      audio.volume = volume;
+      audio.play();
+      setDisplayText(drumPad.id);
+    }
+  };
+
+  const handleVolumeChange = (event) => {
+    const newVolume = parseFloat(event.target.value);
+    setVolume(newVolume);
+  };
+
+  React.useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+  return (
+    <div id="drum-machine" className="container well text-white">
+      <div id="drum-pads" className="row">
+        <div className="col-md-4">
+          <DrumPad id="Heater 1" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" text="Q" onClick={handlePadClick} />
+        </div>
+        <div className="col-md-4">
+          <DrumPad id="Heater 2" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" text="W" onClick={handlePadClick} />
+        </div>
+        <div className="col-md-4">
+          <DrumPad id="Heater 3" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" text="E" onClick={handlePadClick} />
+        </div>
+        <div className="col-md-4">
+          <DrumPad id="Heater 4" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" text="A" onClick={handlePadClick} />
+        </div>
+        <div className="col-md-4">
+          <DrumPad id="Clap" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" text="S" onClick={handlePadClick} />
+        </div>
+        <div className="col-md-4">
+          <DrumPad id="Open-HH" src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" text="D" onClick={handlePadClick} />
+        </div>
+        <div className="col-md-4">
+          <DrumPad id="Kick-n'-Hat" src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" text="Z" onClick={handlePadClick} />
+        </div>
+        <div className="col-md-4">
+          <DrumPad id="Kick" src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" text="X" onClick={handlePadClick} />
+        </div>
+        <div className="col-md-4">
+          <DrumPad id="Closed-HH" src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" text="C" onClick={handlePadClick} />
+        </div>
+      </div>
+      <div id="controller">
+        <div id="display" className="display-4 mb-4 bg-dark text-white">
+          {displayText}
+        </div>
+        <div className="volume-bar">
+          <label htmlFor="volume"> <i className="fas fa-volume-up"></i> Volume</label>
+          <div className="input-group">
+            <input
+              type="range"
+              id="volume"
+              className="form-range"
+              min="0"
+              max="1"
+              step="0.02"
+              value={volume}
+              onChange={handleVolumeChange}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
