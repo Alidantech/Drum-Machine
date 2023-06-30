1d6b606a77f86af469a3566a6e9f62ade2c4f6f1
PETER IRUNGU <peteralidante254@gmail.com>
2023-06-30 15:21:44 +0300

    complete code

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -1,102 +1,108 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const DrumPad = ({ id, src, text, onClick }) => (
-  <div className="drum-pad btn btn-secondary" id={`drum-pad-${id}`} onClick={onClick}>
-    {text}
-    <audio className="clip" id={id} src={src}></audio>
+const drumPadsData = [
+  { id: 'Heater-1', key: 'Q', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3', name: 'Heater 1' },
+  { id: 'Heater-2', key: 'W', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3', name: 'Heater 2' },
+  { id: 'Heater-3', key: 'E', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3', name: 'Heater 3' },
+  { id: 'Heater-4', key: 'A', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3', name: 'Heater 4' },
+  { id: 'Clap', key: 'S', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', name: 'Clap' },
+  { id: 'Open-HH', key: 'D', src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3', name: 'Open-HH' },
+  { id: 'Kick-n\'-Hat', key: 'Z', src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3', name: 'Kick-n\'-Hat' },
+  { id: 'Kick', key: 'X', src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3', name: 'Kick' },
+  { id: 'Closed-HH', key: 'C', src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3', name: 'Closed-HH' }
+];
+
+const DrumPad = ({ padId, keyTrigger, src, name, onClick }) => (
+  <div className="col">
+    <div className="drum-pad btn btn-primary" id={padId} onClick={onClick}>
+      {keyTrigger}
+      <audio className="clip" id={keyTrigger} src={src} />
+    </div>
   </div>
 );
 
-const App = () => {
-  const [displayText, setDisplayText] = useState('');
+function App() {
   const [volume, setVolume] = useState(0.5);
-
-  const handlePadClick = (event) => {
-  const audio = event.target.querySelector('audio');
-  if (!audio.paused) {
-    audio.currentTime = 0;
-  }
-  audio.volume = volume;
-  audio.play();
-  setDisplayText(event.target.id);
-};
-
-const handleKeyDown = (event) => {
-  const drumPad = document.getElementById(`drum-pad-${event.key.toUpperCase()}`);
-  if (drumPad) {
-    const audio = drumPad.querySelector('audio');
-    if (!audio.paused) {
-      audio.currentTime = 0;
-    }
-    audio.volume = volume;
-    audio.play();
-    setDisplayText(drumPad.id);
-  }
-};
+  const [displayText, setDisplayText] = useState('Drum Machine');
 
   const handleVolumeChange = (event) => {
-    const newVolume = parseFloat(event.target.value);
-    setVolume(newVolume);
+    setVolume(event.target.value);
   };
 
-  React.useEffect(() => {
-    document.addEventListener('keydown', handleKeyDown);
+  const handlePadClick = (padId) => {
+    const drumPad = drumPadsData.find((pad) => pad.id === padId);
+    if (drumPad) {
+      playAudio(drumPad.key);
+      setDisplayText(drumPad.name);
+    }
+  };
+
+  const handleKeyDown = (event) => {
+    const drumPad = drumPadsData.find((pad) => pad.key === event.key.toUpperCase());
+    if (drumPad) {
+      playAudio(drumPad.key);
+      setDisplayText(drumPad.name);
+    }
+  };
+
+  const playAudio = (keyTrigger) => {
+  const audioElement = document.getElementById(keyTrigger);
+  if (audioElement && audioElement.paused) {
+    audioElement.currentTime = 0;
+    audioElement.volume = volume;
+    audioElement.play().catch((error) => {
+      console.error('Failed to play audio:', error);
+    });
+  }
+};
+
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
     return () => {
-      document.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keydown', handleKeyDown);
     };
   }, []);
 
   return (
-    <div id="drum-machine" className="container well">
-      <div id="display" className="display-4 mb-4">{displayText}</div>
-      <div className="volume-bar">
-        <label htmlFor="volume">Volume</label>
-        <div className="input-group">
-          <input
-            type="range"
-            id="volume"
-            className="form-range"
-            min="0"
-            max="1"
-            step="0.01"
-            value={volume}
-            onChange={handleVolumeChange}
+    <div id="drum-machine" className="container text-center">
+      <div className="row">
+        {drumPadsData.map((pad) => (
+          <DrumPad
+            key={pad.id}
+            padId={pad.id}
+            keyTrigger={pad.key}
+            src={pad.src}
+            name={pad.name}
+            onClick={() => handlePadClick(pad.id)}
           />
-          <span className="volume-icon"><i className="fas fa-volume-up"></i></span>
-        </div>
+        ))}
       </div>
-      <div id="drum-pads" className="row">
-        <div className="col-md-4">
-          <DrumPad id="Heater 1" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" text="Q" onClick={handlePadClick} />
+      <div className="row">
+        <div className="col">
+          <div id="display" className="mb-4">
+            {displayText}
+          </div>
         </div>
-        <div className="col-md-4">
-          <DrumPad id="Heater 2" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" text="W" onClick={handlePadClick} />
-        </div>
-        <div className="col-md-4">
-          <DrumPad id="Heater 3" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" text="E" onClick={handlePadClick} />
-        </div>
-        <div className="col-md-4">
-          <DrumPad id="Heater 4" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" text="A" onClick={handlePadClick} />
-        </div>
-        <div className="col-md-4">
-          <DrumPad id="Clap" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" text="S" onClick={handlePadClick} />
-        </div>
-        <div className="col-md-4">
-          <DrumPad id="Open-HH" src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" text="D" onClick={handlePadClick} />
-        </div>
-        <div className="col-md-4">
-          <DrumPad id="Kick-n'-Hat" src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" text="Z" onClick={handlePadClick} />
-        </div>
-        <div className="col-md-4">
-          <DrumPad id="Kick" src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" text="X" onClick={handlePadClick} />
-        </div>
-        <div className="col-md-4">
-          <DrumPad id="Closed-HH" src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" text="C" onClick={handlePadClick} />
+        <div className="col">
+          <div className="form-group my-4">
+            <label htmlFor="volumeRange">Volume</label>
+            <input
+              type="range"
+              id="volumeRange"
+              className="form-control-range"
+              min="0"
+              max="1"
+              step="0.01"
+              value={volume}
+              onChange={handleVolumeChange}
+            />
+          </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default App;
